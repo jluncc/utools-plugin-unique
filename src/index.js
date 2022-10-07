6b638488ab60b4818b4a4486d45ed6cce1394e21
jluncc <cjljinglun@163.com>
2022-10-08 00:49:17 +0800

    React重构

---
+++ src/index.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import style from './index.css';
+import MyUtil from './util';
+
+class SourceTextArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.props.handleSourceTextChange(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className={style.left}>
+        <textarea rows="30" cols="45" placeholder="请在该输入框粘贴待去重的文本" 
+          value={this.props.sourceText} onChange={this.handleChange}>
+        </textarea>
+      </div>
+    )
+  }
+}
+
+class ResultTextArea extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className={style.right}>
+        <textarea rows="30" cols="45" placeholder="去重后的结果" defaultValue={this.props.resultText}>
+        </textarea>
+      </div>
+    )
+  }
+}
+
+class CopyBtn extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className={style.sbtnline}>
+        <div>
+          <p>复制文本</p>
+        </div>
+        <div>
+          <button onClick={() => navigator.clipboard.writeText(this.props.sourceText)}>左</button>
+          <button onClick={() => navigator.clipboard.writeText(this.props.resultText)}>右</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+class SortBtn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSort = this.handleSort.bind(this)
+  }
+  handleSort(e) {
+    this.props.handleSortText(e.target.getAttribute('stype'), e.target.getAttribute('stext'));
+  }
+
+  render() {
+    return (
+      <div className={style.sbtnline}>
+        <div>
+          <p>排序文本</p>
+        </div>
+        <div>
+          <div>
+            <button stype="asc" stext={this.props.sourceText} onClick={this.handleSort}>升</button>
+            <button stype="desc" stext={this.props.sourceText} onClick={this.handleSort}>降</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class BtnArea extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className={style.center}>
+        <CopyBtn sourceText={this.props.sourceText} resultText={this.props.resultText} />
+        <SortBtn sourceText={this.props.sourceText} handleSortText={this.props.handleSortText} />
+      </div>
+    )
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSourceTextChange = this.handleSourceTextChange.bind(this);
+    this.handleSortText = this.handleSortText.bind(this);
+    this.state = {
+      sourceText: '',
+      resultText: ''
+    }
+  }
+
+  handleSourceTextChange = (sourceText) => {
+    this.setState({
+      sourceText: sourceText,
+      resultText: MyUtil.removeDuplicateLines(sourceText)
+    })
+  }
+
+  handleSortText = (type, sourceText) => {
+    let afterSortText = MyUtil.sortText(type, sourceText);
+    this.setState({
+      sourceText: afterSortText,
+      resultText: MyUtil.removeDuplicateLines(afterSortText)
+    })
+  }
+
+  render() {
+    return (
+      <div className={style.wrapper}>
+        <SourceTextArea sourceText={this.state.sourceText}
+          handleSourceTextChange={this.handleSourceTextChange} />
+        <BtnArea sourceText={this.state.sourceText} resultText={this.state.resultText} 
+          handleSortText={this.handleSortText} />
+        <ResultTextArea resultText={this.state.resultText} />
+      </div>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('wrapper'));
+root.render(<Wrapper />);
---
+++ src/util.js
@@ -0,0 +1,58 @@
+// 自定义函数
+export default {
+  removeDuplicateLines: function removeDuplicateLines(originalText) {
+    let start = performance.now();
+
+    let lines = originalText.split('\n');
+
+    let linesAfterRemoveDuplicate = new Set();
+    lines.forEach(line => {
+      linesAfterRemoveDuplicate.add(line.trim());
+    })
+
+    let result = this.buildStr(linesAfterRemoveDuplicate);
+    let end = performance.now();
+    console.log('removeDuplicateLines... cost ', `${end - start} ms`);
+    return result;
+  },
+
+  buildStr: function buildStr(lines) {
+    let result = '';
+    lines.forEach(e => {
+      if (e.length > 0) {
+        result += (e + '\n')
+      }
+    })
+    return result;
+  },
+
+  sortText: function sortText(type, text) {
+    // console.log("sortText - type: %s, text: %s", type, text);
+    let start = performance.now();
+
+    let lines = text.split('\n');
+    this.bubbleSort(lines);
+    if ('desc' === type) {
+      lines.reverse();
+    }
+
+    let result = this.buildStr(lines);
+    let end = performance.now();
+    console.log('sortText... cost ', `${end - start} ms`);
+    return result;
+  },
+
+  // TODO 中英数字混合排序待优化，这里将数字当做字符串排序
+  bubbleSort: function bubbleSort(arr) {
+    for (var i = 0; i < arr.length-1; i++) {
+      for (var j = i+1; j < arr.length; j++) {
+        if (arr[i]>arr[j]) {
+          var temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+}
